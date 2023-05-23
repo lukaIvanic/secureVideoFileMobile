1c3e5d85a8ffa75e95e981f7b2efa0b42ef1e38c
Filip Paris <parisfilip2@gmail.com>
2023-05-23 22:49:55 +0200

    defined temporary layouts and test connection to Spring Boot api

--- app/src/main/java/com/example/securevideofile/MainActivity.java
+++ app/src/main/java/com/example/securevideofile/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.securevideofile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,74 +10,43 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonCreate;
-    Button buttonRead;
-    Button buttonUpdate;
-    Button buttonDelete;
 
-    EditText createEditText;
-    EditText updateEditText;
+    Button buttonListOfFilesActivity;
+    Button buttonRetrieveFileActivity;
+    Button buttonUploadFileActivity;
+    Button buttonRetrieveFile;
+    EditText decryptPasswordText;
+    EditText videoNameText;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonCreate = findViewById(R.id.buttonCreate);
-        buttonRead = findViewById(R.id.buttonRead);
-        buttonUpdate = findViewById(R.id.buttonUpdate);
-        buttonDelete = findViewById(R.id.buttonDelete);
+        buttonListOfFilesActivity = findViewById(R.id.buttonListOfFilesActivity);
+        buttonRetrieveFileActivity = findViewById(R.id.buttonRetrieveFileActivity);
+        buttonUploadFileActivity = findViewById(R.id.buttonUploadFileActivity);
 
-        createEditText = findViewById(R.id.createEditText);
-        updateEditText = findViewById(R.id.createEditText);
+        decryptPasswordText = findViewById(R.id.decryptPasswordText);
+        videoNameText = findViewById(R.id.videoNameText);
 
-        buttonCreate.setOnClickListener(v -> {
-
-            String createInput = createEditText.getText().toString();
-            handleCreateClick(createInput);
+        buttonRetrieveFileActivity.setOnClickListener(v -> {
+            setContentView(R.layout.activity_retrieve_file);
         });
-
-        buttonRead.setOnClickListener(v -> {
-
-            handleReadClick();
+        buttonUploadFileActivity.setOnClickListener(v -> {
+            setContentView(R.layout.activity_upload_file);
         });
-
-        buttonUpdate.setOnClickListener(v -> {
-            String updateInput = updateEditText.getText().toString();
-            handleUpdateClick(updateInput);
-        });
-
-        buttonDelete.setOnClickListener(v -> {
-            handleDeleteClick();
-        });
-
-
 
     }
 
-    void handleCreateClick(String createInput){
-        printMessage("Creating todo, " + createInput);
-    }
-
-
-    void handleReadClick(){
+    void handleReadClick(String createInput, String createInput2){
         printMessage("Reading todos");
 
-        new NetworkManager.getTodosAsyncTask().execute(NetworkManager.GET_TODOS_PATH);
-    }
-
-
-    void handleUpdateClick(String updateInput){
-        printMessage("Update todo, " + updateInput);
+        new NetworkManager.getTodosAsyncTask().execute(createInput, createInput2, "POST", "retrieve");
 
     }
-
-
-    void handleDeleteClick(){
-        printMessage("Delete todo");
-
-    }
-
 
     void printMessage(String s){
         Toast.makeText(this, s, Toast.LENGTH_LONG).show();
